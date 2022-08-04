8edd8c801f96915a092af2a4e4c73fd485be301e
Sam De Roeck <sadroeck@gmail.com>
2022-08-04 11:13:34 -0700

    [impl] Simplify observation event handling

--- service/src/ledger.rs
+++ service/src/ledger.rs
@@ -5,7 +5,8 @@ use futures_util::StreamExt;
 use m10_sdk::account::AccountId;
 use m10_sdk::client::Channel;
 use m10_sdk::{
-    AccountFilter, ActionBuilder, Ed25519, M10Client, MetadataExt, StepBuilder, TransferBuilder,
+    AccountFilter, Action, ActionBuilder, Ed25519, M10Client, MetadataExt, StepBuilder, Transfer,
+    TransferBuilder,
 };
 use rust_decimal::Decimal;
 use service::{FxSwapMetadata, FX_SWAP_ACTION};
@@ -43,6 +44,12 @@ impl Ledger {
         })
     }
 
+    async fn get_currencies(&self, request: &Request) -> anyhow::Result<(String, String)> {
+        let from = self.client.get_account(request.from).await?;
+        let to = self.client.get_account(request.to).await?;
+        Ok((from.code.to_lowercase(), to.code.to_lowercase()))
+    }
+
     pub async fn observe_transfers(self, db: LedgerDB) -> anyhow::Result<()> {
         // Sign the request to observe all actions named `FX_SWAP_ACTION`
         let mut transfers = self
@@ -53,21 +60,8 @@ impl Ledger {
 
         while let Some(Ok(transfers)) = transfers.next().await {
             for transfer in transfers {
-                if let Some(payload) = transfer.with_type::<FxSwapMetadata>() {
-                    if let Ok(event) = serde_json::from_slice::<Event>(payload) {
-                        info!(?event);
-                        if let Event::Execute(execute) = event {
-                            launch_swap_task(
-                                self.clone(),
-                                db.clone(),
-                                execute,
-                                transfer.context_id,
-                            )
-                            .await;
-                        } else {
-                            error!("invalid event type");
-                        }
-                    }
+                if let Err(err) = self.handle_transfer(db.clone(), transfer).await {
+                    error!(%err);
                 }
             }
         }
@@ -75,7 +69,70 @@ impl Ledger {
         Ok(())
     }
 
-    pub async fn observe_actions(mut self, db: LedgerDB) -> anyhow::Result<()> {
+    async fn handle_transfer(&self, ledger: LedgerDB, transfer: Transfer) -> anyhow::Result<()> {
+        if let Some(payload) = transfer.with_type::<FxSwapMetadata>() {
+            let event = serde_json::from_slice::<Event>(payload)?;
+            info!(?event);
+            if let Event::Execute(execute) = event {
+                let from = execute.request.from;
+                let to = execute.request.to;
+                let this = self.clone();
+                tokio::spawn(
+                    async move {
+                        info!("Start");
+                        if let Err(err) =
+                            swap_task(this, ledger, execute, transfer.context_id).await
+                        {
+                            error!(%err);
+                        }
+                        info!("Done");
+                    }
+                    .instrument(info_span!("swap", %from, %to)),
+                );
+            } else {
+                error!("invalid event type");
+            }
+        }
+        Ok(())
+    }
+
+    async fn handle_request(&self, db: &LedgerDB, action: Action) -> anyhow::Result<()> {
+        let event = serde_json::from_slice::<Event>(&action.payload)?;
+        info!(?event);
+        let request = match event {
+            Event::Request(request) => request,
+            Event::Quote(_) | Event::Execute(_) | Event::Completed => return Ok(()),
+        };
+        let (from_currency, to_currency) = self.get_currencies(&request).await?;
+        if from_currency != self.currency {
+            return Ok(());
+        }
+        let rate = get_fx_rate(db, &from_currency, &to_currency).await?;
+        let intermediary = db
+            .get(&from_currency)
+            .ok_or_else(|| anyhow::anyhow!("Missing currency"))?
+            .liquidity;
+        let quote = Quote {
+            request,
+            rate,
+            intermediary,
+        };
+        info!(?quote, "Publishing quote");
+        self.client
+            .action(
+                ActionBuilder::for_account(
+                    FX_SWAP_ACTION.to_string(),
+                    self.liquidity,
+                    quote.request.from,
+                )
+                .payload(serde_json::to_vec(&Event::Quote(quote))?),
+                action.context_id,
+            )
+            .await?;
+        Ok(())
+    }
+
+    pub async fn observe_actions(self, db: LedgerDB) -> anyhow::Result<()> {
         // Sign the request to observe all actions named `FX_SWAP_ACTION`
         let mut actions = self
             .client
@@ -84,63 +141,14 @@ impl Ledger {
         info!(action = %FX_SWAP_ACTION, "Started observations");
         while let Some(Ok(actions)) = actions.next().await {
             for action in actions {
-                if let Ok(event) = serde_json::from_slice::<Event>(&action.payload) {
-                    info!(?event);
-                    match event {
-                        Event::Request(request) => {
-                            if let Ok((from_currency, to_currency)) =
-                                get_currencies(&mut self, &request).await
-                            {
-                                if from_currency != self.currency {
-                                    continue;
-                                }
-                                if let Ok(rate) =
-                                    get_fx_rate(&db, &from_currency, &to_currency).await
-                                {
-                                    let intermediary = db.get(&from_currency).unwrap().liquidity;
-                                    let quote = Quote {
-                                        request,
-                                        rate,
-                                        intermediary,
-                                    };
-                                    info!(?quote, "Publishing quote");
-                                    if let Err(err) = self
-                                        .client
-                                        .action(
-                                            ActionBuilder::for_account(
-                                                FX_SWAP_ACTION.to_string(),
-                                                self.liquidity,
-                                                quote.request.from,
-                                            )
-                                            .payload(serde_json::to_vec(&Event::Quote(quote))?),
-                                            action.context_id,
-                                        )
-                                        .await
-                                    {
-                                        error!(%err, "Could not publish quote");
-                                    }
-                                }
-                            }
-                        }
-                        Event::Quote(_) | Event::Execute(_) | Event::Completed => {}
-                    }
+                if let Err(err) = self.handle_request(&db, action).await {
+                    error!(%err);
                 }
             }
         }
 
         Ok(())
     }
-}
-
-async fn get_currencies(
-    ledger: &mut Ledger,
-    request: &Request,
-) -> anyhow::Result<(String, String)> {
-    let from = ledger.client.get_account(request.from).await?;
-    let from_currency = from.code.to_lowercase();
-    let to = ledger.client.get_account(request.to).await?;
-    let to_currency = to.code.to_lowercase();
-    Ok((from_currency, to_currency))
 }
 
 async fn get_fx_rate(
@@ -159,64 +167,58 @@ async fn get_fx_rate(
     Ok(to_ledger.base_rate / from_ledger.base_rate)
 }
 
-async fn launch_swap_task(mut ledger: Ledger, db: LedgerDB, execute: Execute, context_id: Vec<u8>) {
+async fn swap_task(
+    ledger: Ledger,
+    db: LedgerDB,
+    execute: Execute,
+    context_id: Vec<u8>,
+) -> anyhow::Result<()> {
     let mut interval = tokio::time::interval(Duration::from_secs(10));
     let valid_until = SystemTime::UNIX_EPOCH + Duration::from_secs(execute.valid_until);
     let limits = execute.lower_limits..execute.upper_limit;
-    let from = execute.request.from;
-    let to = execute.request.to;
-    let (from_currency, to_currency) = get_currencies(&mut ledger, &execute.request)
-        .await
-        .expect("Could not find FX rate");
-    let to_ledger = db.get(&to_currency).unwrap().clone();
-    tokio::spawn(
-        async move {
-            info!("Start");
-            loop {
-                info!("Polling");
-                if let Ok(rate) = get_fx_rate(&db, &from_currency, &to_currency).await {
-                    let limits_exceeded = !limits.contains(&rate);
-                    let time_exceeded = SystemTime::now() > valid_until;
-                    if limits_exceeded || time_exceeded {
-                        let amount = (execute.request.amount * rate)
-                            .try_into()
-                            .expect("invalid amount");
-                        info!("Executing swap");
-                        to_ledger
-                            .client
-                            .transfer(
-                                TransferBuilder::new().step(StepBuilder::new(
-                                    to_ledger.liquidity,
-                                    to,
-                                    amount,
-                                )),
-                                context_id.clone(),
-                            )
-                            .await
-                            .expect("Could not execute swap");
+    let (from_currency, to_currency) = ledger.get_currencies(&execute.request).await?;
+    let to_ledger = db
+        .get(&to_currency)
+        .ok_or_else(|| anyhow::anyhow!("Missing currency"))?
+        .clone();
 
-                        info!("Publishing completion");
-                        to_ledger
-                            .client
-                            .action(
-                                ActionBuilder::for_account(
-                                    FX_SWAP_ACTION,
-                                    to_ledger.liquidity,
-                                    from,
-                                )
-                                .payload(serde_json::to_vec(&Event::Completed).unwrap()),
-                                context_id,
-                            )
-                            .await
-                            .expect("Could not publish completion");
-                        break;
-                    }
-                }
-                interval.tick().await;
+    loop {
+        info!("Polling");
+        if let Ok(rate) = get_fx_rate(&db, &from_currency, &to_currency).await {
+            let limits_exceeded = !limits.contains(&rate);
+            let time_exceeded = SystemTime::now() > valid_until;
+            if limits_exceeded || time_exceeded {
+                let amount = (execute.request.amount * rate).try_into()?;
+                info!("Executing swap");
+                to_ledger
+                    .client
+                    .transfer(
+                        TransferBuilder::new().step(StepBuilder::new(
+                            to_ledger.liquidity,
+                            execute.request.to,
+                            amount,
+                        )),
+                        context_id.clone(),
+                    )
+                    .await?;
+
+                info!("Publishing completion");
+                to_ledger
+                    .client
+                    .action(
+                        ActionBuilder::for_account(
+                            FX_SWAP_ACTION,
+                            to_ledger.liquidity,
+                            execute.request.from,
+                        )
+                        .payload(serde_json::to_vec(&Event::Completed).unwrap()),
+                        context_id,
+                    )
+                    .await?;
+                break;
             }
-
-            info!("Done");
         }
-        .instrument(info_span!("swap", %from, %to)),
-    );
+        interval.tick().await;
+    }
+    Ok(())
 }
